441693d20d12ca0b6c8a836664ad9525b9c534fe
David <david.cabello.info@gmail.com>
2021-11-29 10:43:45 +0100

    first commit

---
+++ src/main/java/com/dscode/demo/app/DesafioSpringPruebaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dscode.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringPruebaMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringPruebaMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dscode/demo/app/DesafioSpringPruebaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dscode.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringPruebaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
